f5b94a246b4b96180599780e419d6a332406c576
Dmitrii Kim <mitya_kim@icloud.com>
2022-01-12 15:32:39 -0800

    Initial Commit

---
+++ WednesdayWeek9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WednesdayWeek9
+//
+//  Created by Mitya Kim on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
