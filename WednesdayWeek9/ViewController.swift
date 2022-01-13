24b2cb67bb209934d40afc33cd33b381c510a949
Dmitrii Kim <mitya_kim@icloud.com>
2022-01-12 16:06:17 -0800

    final commit

--- WednesdayWeek9/ViewController.swift
+++ WednesdayWeek9/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        aboutPerson()
     }
-
+    
+    func aboutPerson() {
+        nameLabel.text = "Tom Hanks"
+        titleLabel.text = "Is an American actor and filmmaker"
+        ageLabel.text = "Age: 65"
+        bioLabel.text = "Known for both his comedic and dramatic roles, he is one of the most popular and recognizable film stars worldwide, and is regarded as an American cultural icon. Hanks's films have grossed more than $4.9 billion in North America and more than $9.96 billion worldwide, making him the fourth-highest-grossing actor in North America."
+        imageView.image = UIImage(named: "download")
+    }
 
 }
 
